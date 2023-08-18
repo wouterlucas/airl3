31e1e6fd10ea8abd5e22fc2d421b6b34018725ef
Wouter van Boesschoten <wouter@wouterlucas.com>
2023-08-18 07:40:01 +0200

    Have a banana

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,15 @@ import { createSignal, createEffect, on } from "solid-js";
 const Home = () => {
     // const [divOneColor, setdivOneColor] = createSignal('yellow');
 
-    let divOne, divTwo, divThree;
+    let divOne, divTwo, divThree, bananaOne;
     let divs = [];
     let index = 0;
 
     createEffect(() => {
-        divs = [divOne, divTwo, divThree];
+        divs = [divOne, divTwo, divThree, bananaOne];
     });
 
     const move = (direction) => {
-        console.log('move', direction);
-
         const prevDiv = divs[index];
 
         index += direction;
@@ -33,31 +31,39 @@ const Home = () => {
         prevDiv.color = 'blue';
         newDiv.color = 'yellow';
         newDiv.y = 70;
+
+        newDiv.animate({ y: 100 },{ duration: 300 }).start();
     }
         
 
     return (
-      <view>
+        <view onUp={() => console.log(' view on up')}>
           {/* <div lng={{ x: 50, y:50, color:'yellow', h: 100, w: 100 }}  */}
-          <div x={50} y={50} color={'yellow'} h={100} w={100}
-              onBlur={() =>{ console.log('divOne blur') }}
-              onFocus={() => { console.log('divOne focus') }}
+            <div x={50} y={50} color={'yellow'} h={100} w={100}
               onLeft={() => move(-1)}
               onRight={() => move(1)}
               autofocus
               ref={divOne}
             >
-            <p>Hello fucking world.</p>
-          </div>
-          <div id="bla" x={200} y={50} color={'blue'} h={100} w={100}
-            onLeft={() => move(-1)}
-            onRight={() => move(1)}
-            ref={divTwo} />
+                <p>Hello fucking world.</p>
+            </div>
+            <div id="bla" x={200} y={50} color={'blue'} h={100} w={100}
+                onLeft={() => move(-1)}
+                onRight={() => move(1)}
+                ref={divTwo} />
     
-          <div x={350} y={50} color={'blue'} h={100} w={100} 
-            onLeft={() => move(-1)}
-            onRight={() => move(1)}
-            ref={divThree} />
+            <div x={350} y={50} color={'blue'} h={100} w={100} 
+                onLeft={() => move(-1)}
+                onRight={() => move(1)}
+                ref={divThree} />
+
+            <img x={500} y={50} h={100} w={100}  
+                src="/assets/banana.png"
+                onLeft={() => move(-1)}
+                onRight={() => move(1)}
+                ref={bananaOne}
+            />
+        
       </view>
     )
 }
